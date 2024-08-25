1e06c7ab3fab0727445f12fa74559975f2516cfc
wanghaisheng <edwin_uestc@163.com>
2024-08-26 05:27:34 +0800

    test

--- config/db.js
+++ config/db.js
@@ -9,16 +9,19 @@ const DB_URL = 'mongodb://0.0.0.0:27017/shofy';
 const MONGO_URI = secret.db_url;
 
 const connectDB = () => {
+  const dbName = 'test';
   return mongoose
     .connect(MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
+      dbName: dbName
     })
     .then((conn) => {
       console.log(`MongoDB Connected: ${conn.connection.host}`);
+      console.log(`Database Name: ${dbName}`);
       return conn;
     })
-    .catch((error) => {s
+    .catch((error) => {
       console.error(`Error: ${error.message}`);
       process.exit(1);
     });
