3bdf6df6a7a0e879a2b8ec383a2f785f9a46a676
wanghaisheng <edwin_uestc@163.com>
2024-08-26 05:18:49 +0800

    test

--- config/db.js
+++ config/db.js
@@ -9,21 +9,19 @@ const DB_URL = 'mongodb://0.0.0.0:27017/shofy';
 const MONGO_URI = secret.db_url;
 
 const connectDB = () => {
-  return new Promise((resolve, reject) => {
-    mongoose
-      .connect(MONGO_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      })
-      .then((conn) => {
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
-        resolve(conn);
-      })
-      .catch((error) => {
-        console.error(`Error: ${error.message}`);
-        reject(error);
-      });
-  });
+  return mongoose
+    .connect(MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then((conn) => {
+      console.log(`MongoDB Connected: ${conn.connection.host}`);
+      return conn;
+    })
+    .catch((error) => {s
+      console.error(`Error: ${error.message}`);
+      process.exit(1);
+    });
 };
 
 module.exports = connectDB;
